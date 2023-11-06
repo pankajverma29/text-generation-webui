d8b008852649fc5ac99091d940943d90cfbe73e7
Pankaj Verma <pankaj.p.verma@shell.com>
2023-11-06 11:35:08 -0600

    updates

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import autogen
 config_list=[{
-    "api_base":"http://127.0.0.1:5001/v1",
+    "api_base":"http://localhost:1234/v1",
     "api_key":"NULL"
 }]
 
 llm_config={
-    "request_timeout":800,
+    "request_timeout":1000,
     "config_list":config_list}
 
 assistant = autogen.AssistantAgent("assistant", llm_config=llm_config)
